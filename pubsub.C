4f97a03ee52647d044cbbf8b57de1af55bed6311
Steven Smith <sos22@cam.ac.uk>
2014-06-21 16:09:02 -0700

    Extend the pub/sub synchronisation scheme with a way of waiting for IO.

--- pubsub.C
+++ pubsub.C
@@ -1,9 +1,148 @@
 #include "pubsub.H"
 
+#include <sys/poll.h>
+
+#include "fields.H"
+#include "logging.H"
 #include "test.H"
+#include "thread.H"
 #include "timedelta.H"
 
 #include "list.tmpl"
+
+class iopollingthread : public threadfn {
+private: thread *thrd;
+private: mutex_t mux;
+private: bool shutdown;
+private: list<iosubscription *> what;
+private: fd_t readcontrolfd;
+private: fd_t writecontrolfd;
+private: void run();
+public:  void start();
+public:  void attach(iosubscription &);
+public:  void detach(iosubscription &);
+public:  void stop();
+};
+static iopollingthread pollthread;
+
+void
+iopollingthread::run() {
+    int nralloced = 8;
+    struct pollfd *pfds = (struct pollfd *)calloc(sizeof(*pfds), nralloced);
+    auto token(mux.lock());
+    while (!shutdown) {
+        pfds[0] = readcontrolfd.poll(POLLIN);
+        int nr = 1;
+        for (auto it(what.start()); !it.finished(); it.next()) {
+            if (nr == nralloced) {
+                nralloced += 8;
+                pfds = (struct pollfd *)realloc(pfds,
+                                                sizeof(*pfds) * nralloced); }
+            pfds[nr++] = (*it)->fd.poll((*it)->events); }
+        mux.unlock(&token);
+        int r(::poll(pfds, nr, -1));
+        token = mux.lock();
+        if (r < 0) {
+            error::from_errno().fatal(
+                "poll()ing for IO with " + fields::mk(nr) + " fds"); }
+        int i = 0;
+        while (r) {
+            assert(i < nr);
+            if (!pfds[i].revents) {
+                i++;
+                continue; }
+            r--;
+            if (i == 0) {
+                /* Control FD is handled as part of the general loop
+                 * processing. */
+                char b;
+                auto readres(readcontrolfd.read(&b, 1));
+                if (readres.isfailure()) {
+                    readres.failure().fatal("reading poller control FD"); }
+                i++;
+                continue; }
+            /* XXX this is not exactly efficient */
+            bool found = false;
+            for (auto it(what.start()); !it.finished(); it.next()) {
+                auto reg(*it);
+                if (reg->fd.polled(pfds[i])) {
+                    it.remove();
+                    reg->registered = false;
+                    reg->set();
+                    found = true;
+                    break; } }
+            if (!found) {
+                /* This can happen if we race with unregistration in
+                   the wrong way.  It should be pretty rare, though,
+                   so spit out a warning message when it happens. */
+                logmsg(loglevel::debug,
+                       "failed to notify subscriber to fd " +
+                       fields::mk(pfds[i].fd)); }
+            i++; } }
+    mux.unlock(&token);
+    free(pfds); }
+
+void
+iopollingthread::start() {
+    assert(thrd == NULL);
+    assert(!shutdown);
+    auto pr(fd_t::pipe());
+    if (pr.isfailure()) {
+        pr.failure().fatal("creating polling thread control pipe"); }
+    readcontrolfd = pr.success().read;
+    writecontrolfd = pr.success().write;
+    auto r(thread::spawn(this, &thrd, fields::mk("polling thread")));
+    if (r.isjust()) r.just().fatal("starting polling thread"); }
+
+void
+iopollingthread::attach(iosubscription &sub) {
+    auto token(mux.lock());
+    assert(!sub.registered);
+    for (auto it(what.start()); !it.finished(); it.next()) {
+        assert(*it != &sub); }
+    sub.registered = true;
+    what.pushtail(&sub);
+    mux.unlock(&token);
+    auto r(writecontrolfd.write("Y", 1));
+    if (r.isfailure()) {
+        r.failure().fatal("waking up poller thread for new FD"); } }
+
+void
+iopollingthread::detach(iosubscription &sub) {
+    auto token(mux.lock());
+    if (!sub.registered) {
+        /* This can happen sometimes if the iosubscription destructor
+           races with the poll thread notification process.  Ignore it
+           (beyond spitting out a log message, because it should be
+           rare). */
+        mux.unlock(&token);
+        logmsg(loglevel::debug,
+               fields::mk("double unregistered IO subscription"));
+    } else {
+        bool found = false;
+        for (auto it(what.start()); !it.finished(); it.next()) {
+            if (*it == &sub) {
+                it.remove();
+                sub.registered = false;
+                found = true;
+                break; } }
+        assert(found);
+        mux.unlock(&token); }
+    /* Don't wake polling thread up just to tell them to stop
+       listening on this FD; what would be the point? */ }
+
+void
+iopollingthread::stop() {
+    assert(thrd != NULL);
+    auto token(mux.lock());
+    assert(!shutdown);
+    shutdown = true;
+    auto r(writecontrolfd.write("X", 1));
+    mux.unlock(&token);
+    if (r.isfailure()) {
+        r.failure().fatal("writing to poller control FD for shutdown"); }
+    thrd->join();
+    thrd = NULL; }
 
 publisher::publisher()
     : mux(),
@@ -18,27 +157,53 @@ publisher::publish() {
 
 publisher::~publisher() {}
 
-subscription::subscription(subscriber &_sub, publisher &_pub)
+subscriptionbase::subscriptionbase(
+    subscriber &_sub)
     : notified(false),
-      sub(&_sub),
-      pub(&_pub) {
+      sub(&_sub) {
     auto subtoken(sub->mux.lock());
     sub->subscriptions.pushtail(this);
-    sub->mux.unlock(&subtoken);
-    
-    auto pubtoken(pub->mux.lock());
-    pub->subscriptions.pushtail(this);
-    pub->mux.unlock(&pubtoken); }
+    sub->mux.unlock(&subtoken); }
 
 void
-subscription::set() {
+subscriptionbase::set() {
     assert(sub);
-    assert(pub);
     if (!notified) {
         auto tok(sub->mux.lock());
         notified = true;
         sub->set(tok);
         sub->mux.unlock(&tok); } }
+
+subscriptionbase::~subscriptionbase() {
+    bool r = false;
+    auto subtoken(sub->mux.lock());
+    for (auto it(sub->subscriptions.start()); !it.finished(); it.next()) {
+        if (*it == this) {
+            it.remove();
+            r = true;
+            break; } }
+    sub->mux.unlock(&subtoken);
+    assert(r); }
+
+subscription::subscription(subscriber &_sub, publisher &_pub)
+    : subscriptionbase(_sub),
+      pub(&_pub) {
+    auto pubtoken(pub->mux.lock());
+    pub->subscriptions.pushtail(this);
+    pub->mux.unlock(&pubtoken); }
+
+void
+subscription::teardown() {
+    auto pubtoken(pub->mux.lock());
+    bool found = false;
+    for (auto it(pub->subscriptions.start()); !it.finished(); it.next()) {
+        if (*it == this) {
+            it.remove();
+            found = true;
+            break; } }
+    pub->mux.unlock(&pubtoken);
+    assert(found);
+    pub = NULL; }
 
 subscription::~subscription() {
     assert(!!pub == !!sub);
@@ -53,17 +218,44 @@ subscription::~subscription() {
             r = true;
             break; } }
     pub->mux.unlock(&pubtoken);
-    assert(r);
-    
-    r = false;
-    auto subtoken(sub->mux.lock());
-    for (auto it(sub->subscriptions.start()); !it.finished(); it.next()) {
-        if (*it == this) {
-            it.remove();
-            r = true;
-            break; } }
-    sub->mux.unlock(&subtoken);
     assert(r); }
+
+iosubscription::iosubscription(
+    subscriber &_sub,
+    fd_t _fd,
+    int _events)
+    : subscriptionbase(_sub),
+      fd(_fd),
+      events(_events),
+      registered(false) {
+    rearm(); }
+
+void
+iosubscription::rearm() {
+    /* Must have been returned by subscriber::wait(), so can't
+     * currently be registered. */
+    assert(!registered);
+    
+    /* Fast path if we're already notified or the FD is already
+     * ready. */
+    if (notified) return;
+    struct pollfd pfd(fd.poll(events));
+    int r(::poll(&pfd, 1, 0));
+    if (r >= 0 && (pfd.revents & events)) {
+        set();
+        return; }
+    
+    pollthread.attach(*this); }
+
+void
+iosubscription::teardown() {
+    /* This isn't properly synchronised with the IO polling thread, so
+       we could double unregister.  Polling thread is tolerant of
+       that. */
+    if (registered) pollthread.detach(*this); }
+
+iosubscription::~iosubscription() {
+    teardown(); }
 
 void
 subscriber::set(mutex_t::token tok) {
@@ -77,7 +269,7 @@ subscriber::subscriber()
       notified(false),
       subscriptions() {}
 
-subscription *
+subscriptionbase *
 subscriber::wait(maybe<timestamp> deadline) {
     auto token(mux.lock());
     while (1) {
@@ -100,23 +292,20 @@ subscriber::~subscriber() {
     while (!subscriptions.empty()) {
         auto r(subscriptions.pophead());
         assert(r->sub == this);
-        assert(r->pub);
-        
-        auto pubtoken(r->pub->mux.lock());
-        bool found = false;
-        for (auto it(r->pub->subscriptions.start());
-             !found && !it.finished();
-             it.next()) {
-            if (*it == r) {
-                it.remove();
-                found = true; } }
-        r->pub->mux.unlock(&pubtoken);
-        assert(found);
-        
-        r->pub = NULL;
+        r->teardown();
         r->sub = NULL; } }
 
 template class list<subscription *>;
+template class list<iosubscription *>;
+template class list<subscriptionbase *>;
+
+void
+initpubsub() {
+    pollthread.start(); }
+
+void
+deinitpubsub() {
+    pollthread.stop(); }
 
 void
 tests::pubsub(test &support) {
@@ -230,4 +419,39 @@ tests::pubsub(test &support) {
         assert(f != g);
         assert(f == &a || f == &b);
         assert(g == &a || g == &b);
-        assert(h == NULL); } }
+        assert(h == NULL); }
+    support.msg("IO subscriptions");
+    initpubsub();
+    support.detail("basic operation");
+    {   subscriber s;
+        auto p(fd_t::pipe());
+        if (p.isfailure()) p.failure().fatal("creating pipe");
+        iosubscription sub(s, p.success().read, POLLIN);
+        assert(s.wait(timestamp::now() + epsilon) == NULL);
+        p.success().write.write("foo", 3);
+        
+        /* This should arguably be asserting that sub becomes ready
+         * immediately, rather than that it becomes ready after
+         * epsilon, because it is, but that's much harder to implement
+         * and won't matter for any real users of the interface. */
+        assert(s.wait(timestamp::now() + epsilon) == &sub);
+        
+        assert(s.wait(timestamp::now() + epsilon) == NULL);
+        sub.rearm();
+        assert(s.wait(timestamp::now()) == &sub);
+        assert(s.wait(timestamp::now() + epsilon) == NULL);
+        char buf[3];
+        sub.rearm();
+        p.success().read.read(buf, 1);
+        assert(s.wait(timestamp::now()) == &sub);
+        assert(s.wait(timestamp::now() + epsilon) == NULL);
+        sub.rearm();
+        assert(s.wait(timestamp::now()) == &sub);
+        assert(s.wait(timestamp::now() + epsilon) == NULL);
+        sub.rearm();
+        p.success().read.read(buf, 2);
+        assert(s.wait(timestamp::now()) == &sub);
+        assert(s.wait(timestamp::now() + epsilon) == NULL);
+        sub.rearm();
+        assert(s.wait(timestamp::now() + epsilon) == NULL); }
+    deinitpubsub(); }
