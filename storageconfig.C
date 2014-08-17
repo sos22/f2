9d5828bf78d18f9fb9be6bc7fd15a815122454ed
Steven Smith <sos22@cam.ac.uk>
2014-08-16 17:45:24 -0700

    Make the storageslave listen address configurable.

--- storageconfig.C
+++ storageconfig.C
@@ -9,31 +9,37 @@
 storageconfig::storageconfig(const peername &_controlsock,
                              const filename &_poolpath,
                              const beaconclientconfig &_beacon,
-                             const slavename &_name)
+                             const slavename &_name,
+                             const peername &_listenon)
     : controlsock(_controlsock),
       poolpath(_poolpath),
       beacon(_beacon),
-      name(_name) {}
+      name(_name),
+      listenon(_listenon) {}
 
 storageconfig::storageconfig(const quickcheck &q)
     : controlsock(q),
       poolpath(q.filename()),
       beacon(q),
-      name(q) {}
+      name(q),
+      listenon(q) {}
 
 bool
 storageconfig::operator==(const storageconfig &o) const {
     return controlsock == o.controlsock &&
         poolpath == o.poolpath &&
         beacon == o.beacon &&
-        name == o.name; }
+        name == o.name &&
+        listenon == o.listenon; }
 
 const fields::field &
 fields::mk(const storageconfig &sc) {
     return "<storageconfig: controlsock=" + mk(sc.controlsock) +
         " poolpath=" + mk(sc.poolpath) +
         " beacon=" + mk(sc.beacon) +
-        " name=" + mk(sc.name) + ">"; }
+        " name=" + mk(sc.name) +
+        " listenon=" + mk(sc.listenon) +
+        ">"; }
 
 const parser<storageconfig> &
 parsers::_storageconfig() {
@@ -42,17 +48,21 @@ parsers::_storageconfig() {
             ~(" poolpath=" + _filename()) +
             " beacon=" + _beaconclientconfig() +
             " name=" + _slavename() +
+            ~(" listenon=" + _peername()) +
             ">")
-        .map<storageconfig>([] (const pair<pair<pair<maybe<peername>,
-                                                     maybe<filename> >,
-                                                beaconclientconfig>,
-                                           slavename> &x) {
+        .map<storageconfig>([] (const pair<pair<pair<pair<maybe<peername>,
+                                                          maybe<filename> >,
+                                                     beaconclientconfig>,
+                                                slavename>,
+                                           maybe<peername> > &x) {
             return storageconfig(
-                x.first().first().first().dflt(
+                x.first().first().first().first().dflt(
                     peername::local(filename("storageslave"))),
-                x.first().first().second().dflt(filename("storagepool")),
+                x.first().first().first().second().dflt(
+                    filename("storagepool")),
+                x.first().first().second(),
                 x.first().second(),
-                x.second()); }); }
+                x.second().dflt(peername::all(peername::port::any))); }); }
 
 void
 tests::_storageconfig() {
