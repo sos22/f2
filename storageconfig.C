2a8ca0c89c6af61c2396a92c5ccb994754cc1f97
Steven Smith <sos22@srcf.ucam.org>
2015-02-14 22:15:18 -0800

    Storage slaves no longer expose a control interface, so remove support for configuring one.

--- storageconfig.C
+++ storageconfig.C
@@ -11,21 +11,14 @@ mktupledef(storageconfig)
 const parser<storageconfig> &
 parsers::__storageconfig() {
     return ("<storageconfig:" +
-            ~(" controlsock:" + _peername()) +
             ~(" poolpath:" + _filename()) +
             " beacon:" + __beaconserverconfig() +
             ">")
         .map<storageconfig>(
             []
-            (const pair<pair<maybe<peername>,
-                             maybe<filename> >,
-                        beaconserverconfig> &x) {
+            (const pair<maybe<filename>, beaconserverconfig> &x) {
             return storageconfig(
-                x.first().first().dflt(
-                    peername::local(filename("storageslave"))
-                    .fatal("peername storageslave")),
-                x.first().second().dflt(
-                    filename("storagepool")),
+                x.first().dflt(filename("storagepool")),
                 x.second()); }); }
 
 void
