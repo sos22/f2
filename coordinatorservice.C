f50241e33b8392370772c944f76458de69be16f9
Steven Smith <sos22@srcf.ucam.org>
2015-03-01 14:47:12 -0800

    Rework coordinator service so that it calls into an external filesystem service, rather than instantiating its own.

--- coordinatorservice.C
+++ coordinatorservice.C
@@ -2,7 +2,7 @@
 
 #include "connpool.H"
 #include "coordinator.H"
-#include "filesystem.H"
+#include "filesystemproto.H"
 #include "job.H"
 #include "jobname.H"
 #include "logging.H"
@@ -17,12 +17,17 @@
 #include "thread.tmpl"
 
 class coordinatorservice : public rpcservice2 {
-public: filesystem &fs;
+public: agentname &fs;
 public: connpool &pool;
 
 public: class _jobcreator : public thread {
     public: class machine {
-            /* The create job state machine. */
+            /* The create job state machine.  We start by issuing a
+             * NOMINATEAGENT call against the filesystem, and then
+             * once we get the results of that back we move to issuing
+             * CREATEJOB against the storage agent, and then do a
+             * STORAGEBARRIER against the filesystem, and then return
+             * to our own client. */
             /* XXX once we've picked a storage agent to create the job
              * on, we keep trying to create it there until we're told
              * to stop.  That's possibly sub-optimal if e.g. the agent
@@ -34,19 +39,23 @@ public: class _jobcreator : public thread {
         public: rpcservice2::incompletecall &ic;
             /* Connects the subscriber to ic.abandoned().pub() */
         public: maybe<subscription> icsub;
-            /* Event ID at whcih job is created, once it has been
+            /* Event ID at which job is created, once it has been
              * created. */
         public: maybe<proto::eq::eventid> eid;
-            /* Call to the storage agent. */
-        public: connpool::asynccall &call;
+            /* Call to the filesystem or storage agent. */
+        public: connpool::asynccall *call;
             /* Connects the subscriber to call.pub() */
         public: maybe<subscription> callsub;
-            /* Where are we creating the job? */
-        public: const agentname agent;
-            /* What is the job called? */
-        public: const jobname jn;
+            /* Where are we creating the job, once we know (or
+             * mkjust(Nothing) if the filesystem says to give up). */
+        public: maybe<maybe<agentname> > agent;
+            /* Set once we've finished synchronising with the
+             * filesystem. */
+        public: bool synchronised;
+            /* Job we're trying to create. */
+        public: const job j;
         public: machine(rpcservice2::incompletecall &,
-                        const agentname &,
+                        const agentname &_fs,
                         const job &,
                         connpool &,
                         subscriber &); };
@@ -55,12 +64,14 @@ public: class _jobcreator : public thread {
     public: waitbox<void> shutdown;
     public: subscriber sub;
     public: _jobcreator(thread::constoken tok, coordinatorservice &_owner);
-    public: void run(clientio); };
+    public: void run(clientio);
+    public: void fail(machine &m, error why, acquirestxlock);
+    public: void callevt(machine &m, acquirestxlock); };
 public: _jobcreator &jobcreator;
 
 public: coordinatorservice(const constoken &token,
-                           connpool &_pool,
-                           filesystem &_fs)
+                           agentname &_fs,
+                           connpool &_pool)
     : rpcservice2(token, interfacetype::coordinator),
       fs(_fs),
       pool(_pool),
@@ -80,38 +91,19 @@ char
 coordinatorservice::_jobcreator::machine::subscription_call;
 
 orerror<void>
-coordinatorservice::called(clientio io,
+coordinatorservice::called(clientio,
                            deserialise1 &ds,
                            interfacetype t,
                            nnp<incompletecall> ic,
-                           onconnectionthread oct) {
+                           onconnectionthread) {
     assert(t == interfacetype::coordinator);
     proto::coordinator::tag tag(ds);
     if (tag == proto::coordinator::tag::createjob) {
         job j(ds);
         if (ds.isfailure()) return ds.failure();
-        /* Quick fast path to deal with the case where the job already
-         * exists somewhere. */
-        auto already(fs.findjob(j.name()));
-        if (!already.empty()) {
-            ic->complete([res = already.peekhead()]
-                         (serialise1 &s,
-                          mutex_t::token /* txlock */,
-                          onconnectionthread) {
-                             s.push(res); },
-                         acquirestxlock(io),
-                         oct);
-            return Success; }
-        /* Otherwise, start the job creation machine. */
-        auto agent(fs.nominateagent(j.name()));
-        if (agent == Nothing) {
-            /* No agents available -> fail the call. */
-            return error::nostorageagents; }
-        /* Otherwise, start the creation machine to complete the call
-         * asynchronously. */
         assert(!jobcreator.shutdown.ready());
         jobcreator.outstanding.append(*ic,
-                                      agent.just(),
+                                      fs,
                                       j,
                                       pool,
                                       jobcreator.sub);
@@ -127,31 +119,29 @@ coordinatorservice::destroying(clientio io) {
 
 coordinatorservice::_jobcreator::machine::machine(
     rpcservice2::incompletecall &_ic,
-    const agentname &_agent,
-    const job &j,
+    const agentname &_fs,
+    const job &_j,
     connpool &pool,
     subscriber &sub)
     : ic(_ic),
-      icsub(Nothing),
+      icsub(Just(), sub, ic.abandoned().pub, &subscription_ic),
       eid(Nothing),
       call(pool.call(
-               _agent,
-               interfacetype::storage,
+               _fs,
+               interfacetype::filesystem,
                /* Will be cancelled if our client's call gets
                 * cancelled, but not before. */
                Nothing,
-               [j] (serialise1 &s, connpool::connlock) {
-                   s.push(proto::storage::tag::createjob);
-                   s.push(j); },
+               [jn = _j.name()] (serialise1 &s, connpool::connlock) {
+                   s.push(proto::filesystem::tag::nominateagent);
+                   s.push(mkjust(jn)); },
                [this] (deserialise1 &ds, connpool::connlock) {
-                   assert(eid == Nothing);
-                   eid.mkjust(ds);
+                   assert(agent == Nothing);
+                   agent.mkjust(ds);
                    return ds.status(); })),
-      callsub(Nothing),
-      agent(_agent),
-      jn(j.name()) {
-    icsub.mkjust(sub, ic.abandoned().pub, &subscription_ic);
-    callsub.mkjust(sub, call.pub(), &subscription_call); }
+      callsub(Just(), sub, call->pub(), &subscription_call),
+      agent(Nothing),
+      j(_j) { }
 
 coordinatorservice::_jobcreator::_jobcreator(thread::constoken token,
                                              coordinatorservice &_owner)
@@ -162,90 +152,122 @@ coordinatorservice::_jobcreator::_jobcreator(thread::constoken token,
       sub() {}
 
 void
+coordinatorservice::_jobcreator::fail(machine &m,
+                                      error why,
+                                      acquirestxlock atl) {
+    m.callsub = Nothing;
+    if (m.call != NULL) m.call->abort();
+    m.icsub = Nothing;
+    m.ic.fail(why, atl);
+    outstanding.drop(&m); }
+
+void
+coordinatorservice::_jobcreator::callevt(machine &m, acquirestxlock atl) {
+    auto t(m.call->finished());
+    if (t == Nothing) return;
+    m.callsub = Nothing;
+    auto r(m.call->pop(t.just()));
+    m.call = NULL;
+    if (r.isfailure()) {
+        /* Downstream call failed -> upstream call fails as well. */
+        fail(m, r.failure(), atl); }
+    else if (m.agent == Nothing) {
+        /* The agent should have been set by the first successful call. */
+        abort(); }
+    else if (m.agent.just() == Nothing) {
+        /* Filesystem said we have no storage agents.  Fail out. */
+        fail(m, error::nostorageagents, atl); }
+    else if (m.eid == Nothing) {
+        /* Have an agent but not an EID -> time to acquire the EID
+         * from the agent. */
+        m.call = owner.pool.call(
+            m.agent.just().just(),
+            interfacetype::storage,
+            Nothing,
+            [&m] (serialise1 &s, connpool::connlock) {
+                s.push(proto::storage::tag::createjob);
+                s.push(m.j); },
+            [&m] (deserialise1 &ds, connpool::connlock) {
+                assert(m.eid == Nothing);
+                m.eid.mkjust(ds);
+                return ds.status(); });
+        m.callsub.mkjust(sub, m.call->pub(), &machine::subscription_call); }
+    else if (!m.synchronised) {
+        /* We've got a storage agent and an EID -> need to synchronise
+         * with the filesystem. */
+        m.call = owner.pool.call(
+            owner.fs,
+            interfacetype::filesystem,
+            Nothing,
+            [&m] (serialise1 &s, connpool::connlock) {
+                s.push(proto::filesystem::tag::storagebarrier);
+                s.push(m.agent.just().just());
+                s.push(m.eid.just()); },
+            [&m] (deserialise1 &ds, connpool::connlock) {
+                m.synchronised = true;
+                return ds.status(); });
+        m.callsub.mkjust(sub, m.call->pub(), &machine::subscription_call); }
+    else {
+        /* Success! */
+        m.icsub = Nothing;
+        m.ic.complete(
+            [sn = m.agent.just().just()]
+            (serialise1 &s, mutex_t::token /* txlock */) {
+                s.push(sn); },
+            atl);
+        outstanding.drop(&m); } }
+
+void
 coordinatorservice::_jobcreator::run(clientio io) {
     subscription ssub(sub, shutdown.pub);
+    acquirestxlock atl(io);
     while (!shutdown.ready()) {
         auto ss(sub.wait(io));
         if (ss == &ssub) continue;
-        if (ss->data == &machine::subscription_call) {
+        else if (ss->data == &machine::subscription_call) {
             logmsg(loglevel::debug, "woke up for call subscription");
-            auto &m(*containerof(ss, machine, callsub.__just()));
-            auto t(m.call.finished());
-            if (t == Nothing) continue;
-            m.callsub = Nothing;
-            m.icsub = Nothing;
-            auto r(m.call.pop(t.just()));
-            if (r.issuccess()) {
-                /* Make sure the filesystem cache knows about the new
-                 * job before we tell that the client that we're done,
-                 * to avoid stupid races. */
-                /* Note that this is to some extend best effort: if
-                 * the agent drops out of the beacon then we're
-                 * definitely going to lose the newly created job.
-                 * The point is that callers can assume that if the
-                 * job's gone away when we return then something's
-                 * gone wrong; there's no need to wait around for a
-                 * few milliseconds to see if it's going to
-                 * mysteriously reappear. */
-                owner.fs.newjob(m.agent, m.jn, m.eid.just());
-                m.ic.complete([sn = m.agent]
-                              (serialise1 &s, mutex_t::token /* txlock */) {
-                                  s.push(sn); },
-                              acquirestxlock(io)); }
-            else m.ic.fail(r.failure(), acquirestxlock(io));
-            for (auto it(outstanding.start()); true; it.next()) {
-                if (&*it == &m) {
-                    it.remove();
-                    break; } } }
+            callevt(*containerof(ss, machine, callsub.__just()), atl); }
         else if (ss->data == &machine::subscription_ic) {
             logmsg(loglevel::debug, "woke up for ic subscription");
             auto &m(*containerof(ss, machine, icsub.__just()));
             if (!m.ic.abandoned().ready()) continue;
-            m.callsub = Nothing;
-            m.icsub = Nothing;
-            m.call.abort();
-            m.ic.fail(error::shutdown, acquirestxlock(io));
-            for (auto it(outstanding.start()); true; it.next()) {
-                if (&*it == &m) {
-                    it.remove();
-                    break; } } }
+            fail(m, error::shutdown, atl); }
         else abort(); }
     /* We're being shut down.  Abandon anything which is still
      * outstanding. */
     while (!outstanding.empty()) {
-        auto &i(outstanding.peekhead());
-        i.icsub = Nothing;
-        i.callsub = Nothing;
-        i.ic.fail(error::shutdown, acquirestxlock(io));
-        i.call.abort();
-        outstanding.pophead(); } }
+        fail(outstanding.peekhead(), error::shutdown, atl); } }
 
 int
 main(int argc, char *argv[]) {
     initlogging("coordinator");
     initpubsub();
 
-    if (argc != 3) {
-        errx(1, "need two arguments, the cluster to join and our own name"); }
+    if (argc != 4) {
+        errx(1,
+             "need three arguments: the cluster to join, the filesystem "
+             "name, and our own name"); }
     auto cluster(parsers::__clustername()
                  .match(argv[1])
                  .fatal("parsing cluster name " + fields::mk(argv[1])));
     auto name(parsers::_agentname()
               .match(argv[2])
               .fatal("parsing agent name " + fields::mk(argv[2])));
+    auto fs(parsers::_agentname()
+            .match(argv[3])
+            .fatal("parsing agent name " + fields::mk(argv[3])));
     auto bc(beaconclient::build(cluster)
             .fatal("creating beacon client"));
     auto pool(connpool::build(cluster)
               .fatal("creating connection pool"));
-    auto &fs(filesystem::build(pool, *bc));
 
     auto service(rpcservice2::listen<coordinatorservice>(
                      clientio::CLIENTIO,
                      cluster,
                      name,
                      peername::all(peername::port::any),
-                     *pool,
-                     fs)
+                     fs,
+                     *pool)
                  .fatal("listening on coordinator interface"));
 
     while (true) timedelta::hours(1).future().sleep(clientio::CLIENTIO);
