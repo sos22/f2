7c8c772bc539c024669800d6bf56e2e930f32ad0
Steven Smith <sos22@srcf.ucam.org>
2015-04-10 23:42:23 -0700

    Make orerror<X>=error work even when X doesn't have an operator=.

--- testorerror.C
+++ testorerror.C
@@ -17,4 +17,17 @@ tests::_orerror(void) {
             assert(s.issuccess());
             assert(s.success().x == 5);
             assert(!strcmp(s.success().y, "foo"));
-            assert(s.success().z == 1.5); }); }
+            assert(s.success().z == 1.5); });
+    testcaseV("orerror", "failno=", [] {
+            /* orerror<foo> = error::whatever should work even if foo
+             * has no = operator. */
+            class noeq {
+            public: void operator=(const noeq &) = delete; };
+            {   orerror<noeq> base(error::already);
+                assert(base == error::already);
+                base = error::notempty;
+                assert(base == error::notempty); }
+            {   orerror<noeq> base(Success);
+                assert(base.issuccess());
+                base = error::range;
+                assert(base == error::range); } }); }
