e24baa1c805e88cbfb9f1341f5b9fba1b24a2cf2
Steven Smith <sos22@srcf.ucam.org>
2015-04-26 19:58:36 -0700

    Convert the string tests to the new model.

---
+++ tests/unit/teststring.C
@@ -0,0 +1,14 @@
+#include "parsers.H"
+#include "string.H"
+#include "test2.H"
+
+#include "parsers.tmpl"
+#include "test2.tmpl"
+
+static testmodule __teststring(
+    "string",
+    list<filename>::mk("string.C", "string.H"),
+    testmodule::LineCoverage(25_pc),
+    testmodule::BranchCoverage(15_pc),
+    "empty", [] { assert(!strcmp(fields::mk(string("")).c_str(), "\"\"")); },
+    "parser", [] { parsers::roundtrip(string::parser()); });
