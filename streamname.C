5249c90c41c5bf316d692774fc4e88decb694bf2
Steven Smith <sos22@srcf.ucam.org>
2015-05-16 18:13:33 -0700

    The filename and pretty-print versions of streamnames are different enough that they really need independent parsers.

--- streamname.C
+++ streamname.C
@@ -27,36 +27,56 @@ string
 streamname::asfilename() const { return content; }
 
 streamname::streamname(deserialise1 &ds)
-    : content(ds) {
-    if (isvalid()) return;
-    else if (!ds.random()) {
+    : content() {
+    if (ds.random()) mkrandom(ds);
+    else content = string(ds);
+    if (!isvalid()) {
         ds.fail(error::invalidmessage);
-        content = "...badstream..."; }
-    else {
-        auto l = (unsigned)ds % 100 + 1;
-        auto c = (char *)malloc(l+1);
-        static const char validchars[] =
-            "1234567890qwertyuiopasdfghjklzxcvbnm"
-            "QWERTYUIOPASDFGHJKLZXCVBNM[]{};'#:@~,.<>?"
-            "!\"$%^&*()-=_+\\| ";
-        for (unsigned x = 0; x < l; x++) {
-            c[x] = validchars[(unsigned)ds % (sizeof(validchars) - 1)]; }
-        c[l] = '\0';
-        content = string::steal(c);
-        assert(isvalid()); } }
+        content = "...badstream..."; } }
+
+void
+streamname::mkrandom(deserialise1 &ds) {
+    assert(ds.random());
+    auto l = (unsigned)ds % 100 + 1;
+    auto c = (char *)malloc(l+1);
+    static const char validchars[] =
+        "1234567890qwertyuiopasdfghjklzxcvbnm"
+        "QWERTYUIOPASDFGHJKLZXCVBNM[]{};'#:@~,.<>?"
+        "!\"$%^&*()-=_+\\| ";
+    for (unsigned x = 0; x < l; x++) {
+        c[x] = validchars[(unsigned)ds % (sizeof(validchars) - 1)]; }
+    c[l] = '\0';
+    content = string::steal(c);
+    assert(isvalid()); }
+
+streamname::streamname(quickcheck q)
+    : content() {
+    deserialise1 ds(q);
+    mkrandom(ds); }
+
+streamname::streamname(_Steal, streamname &s) : content(Steal, s.content) {}
 
 void
 streamname::serialise(serialise1 &s) const { content.serialise(s); }
 
 const parser<streamname> &
 streamname::parser() {
-    auto &s(string::parser());
-    return (("<stream:" + s + ">") || s)
+    return ("<stream:" + string::parser() + ">")
         .maperr<streamname>(
             [] (const string &x) -> orerror<streamname> {
                 auto r(streamname::mk(x));
                 if (r == Nothing) return error::noparse;
                 else return r.just(); }); }
+
+const parser<streamname> &
+streamname::filenameparser() {
+    class p : public parser<streamname> {
+    private: orerror<result> parse(const char *what) const {
+        orerror<result> res(error::noparse);
+        auto r(streamname::mk(string(what)));
+        if (r != Nothing) res.mksuccess("", Steal, r.just());
+        return res; } };
+    return *new p(); }
 
 unsigned long
 streamname::hash() const { return content.hash(); }
--- tests/unit/teststreamname.C
+++ tests/unit/teststreamname.C
@@ -1,6 +1,7 @@
 #include "streamname.H"
 #include "test2.H"
 
+#include "parsers.tmpl"
 #include "test2.tmpl"
 
 static const testmodule __teststreamname(
@@ -16,4 +17,24 @@ static const testmodule __teststreamname(
         for (unsigned cntr = 0; true; cntr++) {
             streamname s(ds);
             if (!ds.isfailure()) break;
-            assert(cntr < 10); } });
+            assert(cntr < 10); } },
+    "parsers", [] {
+        auto &p(streamname::parser());
+        auto bt([&p] (const char *what) {
+                streamname sn(
+                    streamname::mk(string(what))
+                    .fatal("make streamname for " + fields::mk(what)));
+                assert(p.match(sn.field().c_str()) == sn); });
+        bt("hello");
+        bt("x");
+        parsers::roundtrip<streamname>(p); },
+    "filename", [] {
+        auto &p(streamname::filenameparser());
+        /* Should also be able to parse the filename variant. */
+        quickcheck q;
+        for (unsigned x = 0; x < 100; x++) {
+            deserialise1 ds(q);
+            streamname sn(ds);
+            auto r(p.match(sn.asfilename()));
+            assert(r == sn); } }
+);
