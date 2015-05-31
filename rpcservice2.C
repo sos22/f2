923033af076c25cafd9c87b135e4634a02612f1b
Steven Smith <sos22@srcf.ucam.org>
2015-05-30 22:24:00 -0700

    Fix a stupid use-after-free in rpcservice2.  root has already been joined by the time we get to the destructor, so will be freed and we can't check if it's been shut down.

--- rpcservice2.C
+++ rpcservice2.C
@@ -239,8 +239,7 @@ rpcservice2::destroy(clientio io) {
 void
 rpcservice2::destroying(clientio) {}
 
-rpcservice2::~rpcservice2() {
-    assert(root->shutdown.ready()); }
+rpcservice2::~rpcservice2() {}
 
 tests::hookpoint<void>
 rpcservice2::clientdisconnected([] { } );
