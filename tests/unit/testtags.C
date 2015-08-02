661918c3ecc491a7886d9bae1de5f73bef55ce6b
Steven Smith <sos22@srcf.ucam.org>
2015-08-01 20:36:18 -0700

    Whitelist a couple of files which don't need testing.

---
+++ tests/unit/testtags.C
@@ -0,0 +1,8 @@
+#include "test2.H"
+
+#include "test2.tmpl"
+
+/* Dummy test module for the tag types, since they have no code. */
+static const testmodule __testtags(
+    "tagtypes",
+    list<filename>::mk("clientio.H", "empty.H", "steal.H"));
