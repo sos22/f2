1c4916913ffa8128d5d53b57b98e063dd72a159c
Steven Smith <sos22@srcf.ucam.org>
2015-08-14 22:03:07 -0700

    Use computeclient from computecli, rather than reimplementing it.

--- computecli.C
+++ computecli.C
@@ -3,6 +3,7 @@
 #include "agentname.H"
 #include "clustername.H"
 #include "compute.H"
+#include "computeclient.H"
 #include "connpool.H"
 #include "eq.H"
 #include "fields.H"
@@ -30,62 +31,36 @@ main(int argc, char *argv[]) {
     auto peer(parsers::_agentname()
               .match(argv[2])
               .fatal("parsing agent name " + fields::mk(argv[2])));
-    auto pool(connpool::build(cluster).fatal("building conn pool"));
+    auto &pool(*connpool::build(cluster).fatal("building conn pool"));
+    auto &client(computeclient::connect(pool, peer));
     if (!strcmp(argv[3], "START")) {
         if (argc != 5) errx(1, "START mode needs a job argument");
         auto j(job::parser()
                .match(argv[4])
                .fatal("parsing job " + fields::mk(argv[4])));
-        maybe<proto::eq::eventid> eid(Nothing);
-        maybe<proto::compute::tasktag> tag(Nothing);
-        pool->call(clientio::CLIENTIO,
-                   peer,
-                   interfacetype::compute,
-                   Nothing,
-                   [&j] (serialise1 &s, connpool::connlock) {
-                       s.push(proto::compute::tag::start);
-                       s.push(j); },
-                   [&eid, &tag] (deserialise1 &ds, connpool::connlock) {
-                       eid.mkjust(ds);
-                       tag.mkjust(ds);
-                       return ds.status(); })
-            .fatal("calling compute agent START method");
-        fields::print("result " + tag.just().field() +
-                      " at " + eid.just().field() + "\n"); }
+        fields::print("result " +
+                      client
+                      .start(clientio::CLIENTIO, j)
+                      .fatal("starting job")
+                      .field()); }
     else if (!strcmp(argv[3], "ENUMERATE")) {
         if (argc != 4) errx(1, "ENUMERATE has no arguments");
-        maybe<proto::eq::eventid> eid(Nothing);
-        maybe<list<proto::compute::jobstatus> > res(Nothing);
-        pool->call(clientio::CLIENTIO,
-                   peer,
-                   interfacetype::compute,
-                   Nothing,
-                   [] (serialise1 &s, connpool::connlock) {
-                       s.push(proto::compute::tag::enumerate); },
-                   [&eid, &res] (deserialise1 &ds, connpool::connlock) {
-                       eid.mkjust(ds);
-                       res.mkjust(ds);
-                       return ds.status(); })
-            .fatal("calling compute agent ENUMERATE method");
-        fields::print("result " + fields::mk(res.just()) +
-                      " at " + eid.just().field() + "\n"); }
+        fields::print("result " +
+                      client
+                      .enumerate(clientio::CLIENTIO)
+                      .fatal("enumerating jobs")
+                      .field()); }
     else if (!strcmp(argv[3], "DROP")) {
         if (argc != 5) {
             errx(1, "DROP mode takes a single argument, the job name to drop");}
         auto j(jobname::parser()
                .match(argv[4])
                .fatal("parsing job " + fields::mk(argv[4])));
-        pool->call(clientio::CLIENTIO,
-                   peer,
-                   interfacetype::compute,
-                   Nothing,
-                   [&j] (serialise1 &s, connpool::connlock) {
-                       s.push(proto::compute::tag::drop);
-                       s.push(j); },
-                   [] (deserialise1 &ds, connpool::connlock) {
-                       return ds.status(); })
-            .fatal("calling compute agent DROP method"); }
+        client
+            .drop(clientio::CLIENTIO, j)
+            .fatal("dropping job"); }
     else {
         errx(1, "unknown mode %s", argv[3]); }
-    pool->destroy();
+    client.destroy();
+    pool.destroy();
     deinitpubsub(clientio::CLIENTIO); }
