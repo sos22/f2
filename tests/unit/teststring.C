5a78510862b299078cafe924019245f3da7c352f
Steven Smith <sos22@srcf.ucam.org>
2015-08-30 14:45:44 -0700

    Restore coverage numbers on string module.

--- tests/unit/teststring.C
+++ tests/unit/teststring.C
@@ -8,7 +8,12 @@
 static testmodule __teststring(
     "string",
     list<filename>::mk("string.C", "string.H"),
-    testmodule::LineCoverage(25_pc),
-    testmodule::BranchCoverage(17_pc),
+    testmodule::LineCoverage(54_pc),
+    testmodule::BranchCoverage(31_pc),
     "empty", [] { assert(!strcmp(fields::mk(string("")).c_str(), "\"\"")); },
-    "parser", [] { parsers::roundtrip(string::parser()); });
+    "parser", [] { parsers::roundtrip(string::parser()); },
+    "concat", [] {
+        string x = string("foo") + string("bar");
+        assert(x == "foobar");
+        x += "bazz";
+        assert(x == "foobarbazz"); });
