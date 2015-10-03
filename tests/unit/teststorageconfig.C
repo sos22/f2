5fca1fdb5db74b46300dd07ff5aa4f2cd47b2699
Steven Smith <sos22@srcf.ucam.org>
2015-10-02 21:57:18 -0700

    Restore coverage on teststorageconfig and teststreamname.

--- tests/unit/teststorageconfig.C
+++ tests/unit/teststorageconfig.C
@@ -2,10 +2,14 @@
 #include "test2.H"
 
 #include "parsers.tmpl"
+#include "serialise.tmpl"
 #include "test2.tmpl"
 
 static testmodule __teststorageconfig(
     "storageconfig",
     list<filename>::mk("storageconfig.C", "storageconfig.H"),
     testmodule::BranchCoverage(50_pc),
-    "parsers", [] { parsers::roundtrip(storageconfig::parser()); });
+    "parsers", [] { parsers::roundtrip(storageconfig::parser()); },
+    "serialise", [] {
+        quickcheck q;
+        serialise<storageconfig>(q); } );
