264c1cde892519542b501d3dd8c5ac98d59ea5ce
Steven Smith <sos22@srcf.ucam.org>
2015-10-10 23:06:53 -0700

    Don't want core dumps from the abort helper.

--- tests/abort/abort.c
+++ tests/abort/abort.c
@@ -1,6 +1,14 @@
 /* Trivial program which just calls abort(), for testing. */
+#include <sys/resource.h>
+#include <sys/time.h>
 #include <stdlib.h>
+#include <string.h>
 
 int
 main() {
+    /* Core dumps from this program are just noise, and tend to
+     * confuse the harness, so turn them off. */
+    struct rlimit rl;
+    memset(&rl, 0, sizeof(rl));
+    if (setrlimit(RLIMIT_CORE, &rl) < 0) err("setrlimit");
     abort(); }
