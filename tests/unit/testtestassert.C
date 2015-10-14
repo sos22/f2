e7756af5c419d496ebea39c2e81ed262aaa3f327
Steven Smith <sos22@srcf.ucam.org>
2015-10-13 22:36:54 -0700

    Introduce a new sort of assertion, testasserts, which makes it a bit easier to capture potentially-useful information in a test assertion without needing extra logging,
    This is basically a kind of lazy expression capture thing, allowing us
    to do some actual metaprogramming for a change. The only moderately
    clever part is making sure that x&&y does the right kind of short
    circuiting, which is usually a bit tricky with C++ operator
    overloading.
    
    I've only done overloads for a couple of operators here. I figure we
    can add the later when they're actually needed; it's not very hard.

---
+++ tests/unit/testtestassert.C
@@ -0,0 +1,73 @@
+#include "logging.H"
+#include "testassert.H"
+#include "test2.H"
+
+#include "testassert.tmpl"
+#include "test2.tmpl"
+
+static testmodule __testcomputeagent(
+    "testassert",
+    list<filename>::mk("testassert.H", "testassert.tmpl"),
+    testmodule::BranchCoverage(50_pc),
+    "values", [] {
+        {   auto &t(T(5));
+            assert(!strcmp(t.field().c_str(), "5"));
+            assert(t.eval() == 5);
+            assert(!strcmp(t.field().c_str(), "5"));
+            delete &t; }
+        {   auto &t(T('a'));
+            assert(!strcmp(t.field().c_str(), "a"));
+            assert(t.eval() == 'a');
+            assert(!strcmp(t.field().c_str(), "a"));
+            delete &t; }
+        {   auto &t(T(7.25));
+            assert(!strcmp(t.field().c_str(), "7.25"));
+            assert(t.eval() == 7.25);
+            assert(!strcmp(t.field().c_str(), "7.25"));
+            delete &t; }
+        {   int x;
+            x = 92;
+            auto &t(T(x));
+            assert(!strcmp(t.field().c_str(), "x{...}"));
+            x = 97;
+            assert(!strcmp(t.field().c_str(), "x{...}"));
+            assert(t.eval() == 97);
+            assert(!strcmp(t.field().c_str(), "x{97}"));
+            x = 93;
+            assert(!strcmp(t.field().c_str(), "x{97}"));
+            delete &t; } },
+    "expressions", [] {
+        {   int y = 5;
+            auto &t(T(y) + T(7));
+            assert(!strcmp(t.field().c_str(), "(y{...} + 7)"));
+            assert(t.eval() == 12);
+            assert(!strcmp(t.field().c_str(), "(y{5} + 7)"));
+            delete &t; }
+        {   int y = 5;
+            int x = 7;
+            auto &t(T(y) > T(x));
+            assert(!strcmp(t.field().c_str(), "(y{...} > x{...})"));
+            assert(t.eval() == false);
+            assert(!strcmp(t.field().c_str(), "(y{5} > x{7})"));
+            delete &t; }
+        {   int y = 5;
+            int x = 7;
+            auto &t(T(x) > T(y));
+            assert(!strcmp(t.field().c_str(), "(x{...} > y{...})"));
+            assert(t.eval() == true);
+            assert(!strcmp(t.field().c_str(), "(x{7} > y{5})"));
+            delete &t; }
+        {   bool f = false;
+            bool t = true;
+            auto &tt(T(f) && T(t));
+            assert(!strcmp(tt.field().c_str(), "(f{...} && t{...})"));
+            assert(tt.eval() == false);
+            assert(!strcmp(tt.field().c_str(), "(f{FALSE} && t{...})"));
+            delete &tt; }
+        {   bool f = false;
+            bool t = true;
+            auto &tt(T(t) && T(f));
+            assert(!strcmp(tt.field().c_str(), "(t{...} && f{...})"));
+            assert(tt.eval() == false);
+            assert(!strcmp(tt.field().c_str(), "(t{TRUE} && f{FALSE})"));
+            delete &tt; } } );
