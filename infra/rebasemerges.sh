21b4183c7d2bb4c1057bb10df45bdc786e5760ea
Steven Smith <sos22@srcf.ucam.org>
2015-10-17 09:56:37 -0700

    Nicer log messages in a couple of the helper scripts.

--- infra/rebasemerges.sh
+++ infra/rebasemerges.sh
@@ -3,17 +3,28 @@
 set -e
 
 startat=$(git symbolic-ref --short -q HEAD)
-trap "git checkout $startat" EXIT
-git checkout HEAD^
+trap "git checkout -q $startat 2> /dev/null" EXIT
+git checkout -q HEAD^
 git fetch arnold:f2 master:master
 git branch | grep ' merge/' | while read name
                               do
-                                  git checkout $name
+                                  git checkout -q $name
                                   if ! git rebase master
                                   then
                                       git rebase --abort
                                       continue
                                   fi
-                                  git checkout master
-                                  git branch -d $name || true
+                                  git checkout -q master
+                                  git branch -d $name 2>/dev/null || true
                               done
+
+echo
+
+if git branch | grep -q ' merge/'
+then
+    echo "Merges still pending: "
+    git branch | grep ' merge/' | sed 's,  merge/,,'
+else
+    echo "All branches merged"
+fi
+
--- infra/repushmerge.sh
+++ infra/repushmerge.sh
@@ -3,14 +3,14 @@
 set -e
 
 startat=$(git symbolic-ref --short -q HEAD)
-trap "git checkout $startat" EXIT
-git checkout HEAD^
+trap "git checkout -q $startat" EXIT
+git checkout -q HEAD^
 git fetch arnold:f2 master:master
 git branch | grep ' merge/' | while read name
                               do
-                                  git checkout $name
+                                  git checkout -q $name
                                   git rebase master
-                                  git checkout master
-                                  git branch -d $name && continue
+                                  git checkout -q master
+                                  git branch -d $name 2>/dev/null && continue
                                   git push arnold:f2 ${name}:${name}
                               done
