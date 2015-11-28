2ff0a70ed4334b1c4f39bf7e8b4542d1dcd0244b
Steven Smith <sos22@srcf.ucam.org>
2015-11-28 15:21:05 -0800

    Rip out the old coordinator. It never really did anything useful, and it doesn't really make sense any more.

--- interfacetype.C
+++ interfacetype.C
@@ -20,25 +20,22 @@ interfacetype::storage(5);
 const interfacetype
 interfacetype::eq(6);
 const interfacetype
-interfacetype::coordinator(7);
+interfacetype::compute(7);
 const interfacetype
-interfacetype::compute(8);
-const interfacetype
-interfacetype::filesystem(9);
+interfacetype::filesystem(8);
 
 interfacetype::interfacetype(unsigned _v)
     : v(_v) {}
 
 interfacetype::interfacetype(quickcheck &q) {
-    switch ((unsigned)q % 8) {
+    switch ((unsigned)q % 7) {
     case 0: *this = meta; break;
     case 1: *this = storage; break;
     case 2: *this = eq; break;
     case 3: *this = test; break;
     case 4: *this = test2; break;
-    case 5: *this = coordinator; break;
-    case 6: *this = compute; break;
-    case 7: *this = filesystem; break; } }
+    case 5: *this = compute; break;
+    case 6: *this = filesystem; break; } }
 
 interfacetype::interfacetype(deserialise1 &ds)
     : v(ds) {
@@ -47,7 +44,6 @@ interfacetype::interfacetype(deserialise1 &ds)
         *this != meta &&
         *this != storage &&
         *this != eq &&
-        *this != coordinator &&
         *this != compute &&
         *this != filesystem) {
         ds.fail(error::invalidmessage);
@@ -64,7 +60,6 @@ interfacetype::field() const {
     else if (*this == eq) return fields::mk("eq");
     else if (*this == test) return fields::mk("test");
     else if (*this == test2) return fields::mk("test2");
-    else if (*this == coordinator) return fields::mk("coordinator");
     else if (*this == compute) return fields::mk("compute");
     else if (*this == filesystem) return fields::mk("filesystem");
     else return "<bad type " + fields::mk(v) + ">"; }
@@ -76,6 +71,5 @@ interfacetype::parser() {
         strmatcher("eq", eq) ||
         strmatcher("test2", test2) ||
         strmatcher("test", test) ||
-        strmatcher("coordinator", coordinator) ||
         strmatcher("compute", compute) ||
         strmatcher("filesystem", filesystem); }
