21f8997c776150b165c87d7bd4c2bb0ffde839d7
Steven Smith <sos22@srcf.ucam.org>
2015-12-06 15:48:36 -0800

    Move condpingpong into testpubsub, rather than its own special program.

--- tests/unit/testpubsub.C
+++ tests/unit/testpubsub.C
@@ -414,9 +414,8 @@ static testmodule __testpubsub(
                 pipe.fd1.readpoll(b, sizeof(b)).fatal("readpoll"); }
             (timestamp::now() + timedelta::milliseconds(100)).sleep(_io);
             assert(sub.poll() == &writing); }
-        deinitpubsub(_io); }
+        deinitpubsub(_io); },
 #if TESTING
-    ,
     "publots", [] {
         /* A single subscriber which subscribes to lots of publishers
          * should produce some warning messages (and also work). */
@@ -464,6 +463,51 @@ static testmodule __testpubsub(
             for (auto x = 0; x < nrsubs; x++) assert(subs[x].wait(_io)==&ss[x]);
             for (auto x = 0; x < nrsubs; x++) ss[x].~subscription();
             free(ss); }
-        deinitpubsub(_io); }
+        deinitpubsub(_io); },
 #endif
+    testmodule::TestFlags::noauto(), "condpingpong", [] {
+        auto takesample([] (clientio io) {
+                pthread_mutex_t mux;
+                pthread_mutex_init(&mux, NULL);
+                pthread_cond_t cond;
+                pthread_cond_init(&cond, NULL);
+                unsigned long cntr = 0;
+                bool done = false;
+                unsigned phase = 0;
+                unsigned long start;
+                unsigned long end;
+                {   spark<void> thr1([&] {
+                            assert(phase == 0);
+                            pthread_mutex_lock(&mux);
+                            while (!done) {
+                                cntr++;
+                                phase = 1;
+                                pthread_cond_signal(&cond);
+                                while (phase == 1 && !done) {
+                                    pthread_cond_wait(&cond, &mux); } }
+                            pthread_mutex_unlock(&mux); });
+                    spark<void> thr2([&] {
+                            pthread_mutex_lock(&mux);
+                            while (!done) {
+                                while (phase != 1 && !done) {
+                                    pthread_cond_wait(&cond, &mux); }
+                                phase = 2;
+                                pthread_cond_signal(&cond); }
+                            pthread_mutex_unlock(&mux); });
+                    /* Give it a second to get started. */
+                    (1_s).future().sleep(io);
+                    start = __sync_fetch_and_add(&cntr, 0);
+                    /* And then let it run for a bit. */
+                    (10_s).future().sleep(io);
+                    end = __sync_fetch_and_add(&cntr, 0);
+                    done = true;
+                    pthread_cond_broadcast(&cond); }
+                pthread_cond_destroy(&cond);
+                pthread_mutex_destroy(&mux);
+                return end - start; });
+        list<unsigned long> samples;
+        for (unsigned cntr = 0; cntr < 5; cntr++) {
+            auto s(takesample(clientio::CLIENTIO));
+            logmsg(loglevel::info, "sample " + fields::mk(s));
+            samples.pushtail(s); } }
     );
