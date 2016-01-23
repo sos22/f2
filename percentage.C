83586a3ce2a2186217523aaff2a664c5be188c59
Steven Smith <sos22@srcf.ucam.org>
2016-01-23 13:37:22 -0800

    Get rid of parser maperr in percentage parser.

--- percentage.C
+++ percentage.C
@@ -15,5 +15,11 @@ percentage::field() const { return fields::mk(val) + "%"; }
 
 const parser<percentage> &
 percentage::parser() {
-    return (parsers::longdoubleparser + "%").map<percentage>(
-        [] (long double x) { return percentage(x); }); }
+    class f : public ::parser<percentage> {
+    public: const ::parser<long double> &inner;
+    public: f() : inner(parsers::longdoubleparser + "%") {}
+    public: orerror<result> parse(const char *what) const {
+        return inner.parse(what).map<result>([] (auto r) {
+                return r.map<percentage>([] (auto x) {
+                        return percentage(x); }); }); } };
+    return *new f(); }
