047ff36f0fb24561b1e7c62a0768ed4912347caa
Steven Smith <sos22@srcf.ucam.org>
2016-02-13 17:56:30 -0800

    dd sometimes trips up Valgrind, so don't trace it.

--- infra/runvalgrind.sh
+++ infra/runvalgrind.sh
@@ -6,5 +6,5 @@ shift
 # Run normal memcheck valgrind with all of our usual settings.
 valgrind --tool=memcheck --leak-check=full --error-exitcode=2 --trace-children=yes \
          --log-file=${LOGFILE} \
-         --trace-children-skip=/usr/bin/make,/bin/mktemp,/usr/bin/cmp \
+         --trace-children-skip=/usr/bin/make,/bin/mktemp,/usr/bin/cmp,/bin/dd \
          "$@"
